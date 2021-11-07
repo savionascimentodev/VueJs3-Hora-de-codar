e57609165b89a2ef817082c01ca11304e665e428
savionascimentodev <savionascimentodev@gmail.com>
2021-11-06 21:52:51 -0400

    ✨ completed class #22

---
+++ make-your-burguer/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="footer">
+    <p>Make Your Burguer &copy; 2021</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+#footer {
+  height: 250px;
+  background-color: #222;
+  box-shadow: -1px -7px 17px 2px rgba(0, 0, 0, 0.5);
+  color: #fdba03;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ make-your-burguer/src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <router-link to="/" class="logoUrl">
+      <img :src="logo" :alt="alt" id="logo" />
+    </router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/pedidos">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: ['logo', 'alt']
+}
+</script>
+
+<style scoped>
+#nav {
+  background-color: #222;
+  box-shadow: -1px 8px 17px 2px rgba(0, 0, 0, 0.5);
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+#nav .logoUrl {
+  margin: auto;
+  margin-left: 0px;
+}
+
+#logo {
+  height: 40px;
+  width: 40px;
+}
+
+#nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
+}
+
+#nav a:hover {
+  color: #fff;
+}
+</style>
