8e6020e50a88c527fe95e43153d6d0461f504e6b
savionascimentodev <savionascimentodev@gmail.com>
2021-11-23 23:33:06 -0400

    ✨ completed class #29

--- make-your-burguer/src/components/Dashboard.vue
+++ make-your-burguer/src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="burguer-table">
+  <div id="burger-table">
     <div>
-      <div id="burguer-table-header">
+      <div id="burger-table-heading">
         <div class="order-id">#:</div>
         <div>Cliente:</div>
         <div>Pão:</div>
@@ -10,24 +10,25 @@
         <div>Ações:</div>
       </div>
     </div>
-    <div class="burguer-table-rows">
-      <div class="burguer-table-rows">
-        <div class="order-number">
-          <div>Jão</div>
-          <div>Pão</div>
-          <div>Maminha</div>
-          <div>
-            <ul>
-              <li>Salame</li>
-              <li>Tomate</li>
-            </ul>
-          </div>
-          <div>
-            <select name="status" class="status">
-              <option value="">Selecione o status do hamburguer</option>
-            </select>
-            <button classs="btn-delete">Cancelar</button>
-          </div>
+
+    <div id="burger-table-rows">
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.name }}</div>
+        <div>{{ burger.bread }}</div>
+        <div>{{ burger.meat }}</div>
+        <div>
+          <ul>
+            <li v-for="(options, index) in burger.options" :key="index">
+              {{ options }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option>Opção1</option>
+          </select>
+          <button class="delete-btn">Cancelar</button>
         </div>
       </div>
     </div>
@@ -36,10 +37,83 @@
 
 <script>
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  data() {
+    return {
+      burgers: null,
+      burgerId: null,
+      status: []
+    }
+  },
+  mounted() {
+    this.getPedidos()
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch('http://localhost:3000/burgers')
+      const data = await req.json()
+
+      this.burgers = data
+      console.log(this.burgers)
+      // Resgatar status
+    }
+  }
 }
 </script>
 <style scoped>
-#burguer-table {
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+
+#burger-table-heading div,
+.burger-table-row div {
+  width: 19%;
+}
+
+#burger-table-heading .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
+
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
 }
 </style>
